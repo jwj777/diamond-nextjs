9bd5deea9bad9752ea5d8abc43318e4c2dad1f0c
Webster <webster.jorgensen@gmail.com>
2024-05-28 11:48:01 -0500

    header and submit card form

--- src/app/_components/form/SubmitCardForm.js
+++ src/app/_components/form/SubmitCardForm.js
@@ -1,132 +1,390 @@
-'use client'
-import { Box, Image, Text, Button, Select } from "@chakra-ui/react";
+"use client";
+import {
+  Box,
+  Image,
+  Text,
+  Button,
+  Select,
+  Toast,
+  Grid,
+  GridItem,
+} from "@chakra-ui/react";
 import TitleLarge from "../typography/TitleLarge";
 import HeadlineLarge from "../typography/HeadlineLarge";
 import BodyLarge from "../typography/BodyLarge";
 import BodyMedium from "../typography/BodyMedium";
 import InputFloat from "./inputFloat";
-import { useState } from "react";
-
+import { useState, useEffect } from "react";
+import { fees, prices } from "@/app/priceData";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useShoppingCart } from "use-shopping-cart";
+import { loadStripe } from "@stripe/stripe-js";
 
 export default function SubmitCardForm({ data }) {
+  const [subscriptions, setSubscriptions] = useState([]);
+  const { addItem, cartDetails, clearCart, cartCount, formattedTotalPrice } =
+    useShoppingCart();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const [formSubmit, setFormSubmit] = useState(false);
+  const [name, setName] = useState("");
+  const [brand, setBrand] = useState("");
+  const [year, setYear] = useState();
+  const [number, setNumber] = useState();
+  const [desc, setDesc] = useState("");
+  const [value, setValue] = useState();
+  const [quantity, setQuantity] = useState();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const { user, isLoading } = useUser();
 
-    !formSubmit ? setFormSubmit(true) : setFormSubmit(false)
+  useEffect(() => {
+    if (!isLoading && !user) {
+      return redirect("/");
+    }
+    setLoading(true);
+    async function fetchSubscriptions() {
+      try {
+        const customerRes = await fetch(
+          `/api/stripe/customer?email=${user.email}`
+        );
+        const customerData = await customerRes.json();
 
-    const data = {
-      firstName: e.target.firstName.value,
-      lastName: e.target.lastName.value,
-      company: e.target.company.value,
-      email: e.target.email.value,
-      phone: e.target.phone.value
+        const response = await fetch(
+          `/api/stripe/subscriptions?customerId=${customerData.id}`
+        );
+        const data = await response.json();
+        setSubscriptions(data);
+
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+      }
     }
 
-    // Send the data to the server in JSON format.
-    const JSONdata = JSON.stringify(data);
+    fetchSubscriptions();
+  }, [user, isLoading]);
 
-    // API endpoint where we send form data.
-    // const endpoint = 'https://hooks.zapier.com/hooks/catch/8026392/3fi7xo8/';
+  const addToCart = async () => {
+    if(!subscriptions.length) {
+      alert("Please subscribe the membership first.");
+      return;
+    }
+    if (!name || !brand || !year || !number || !value || !quantity) {
+      alert("Please input all fields.");
+      return;
+    }
+    const levels = Object.keys(fees);
+    const levelIdx =
+      levels.findIndex((item) => parseInt(item) > parseInt(value)) - 1;
+    const level = levels[levelIdx];
+    console.log(
+      levels,
+      levelIdx,
+      value,
+      level,
+      fees[level],
+      subscriptions[0].product.name
+    );
 
-    // Form the request for sending data to the server.
-    const options = {
-      method: 'POST',
-      body: JSONdata,
+    const fee = fees[level][subscriptions[0].product.name];
+    const price =
+      prices[level][subscriptions[0].product.name] * 100 +
+      fee * (parseInt(value) - parseInt(level));
+    const product = {
+      name,
+      description: desc,
+      id: "prod_Q90vXwIVPSesQV" + new Date().getTime(),
+      price,
+      currency: "USD",
     };
+    addItem(product, {
+      count: parseInt(quantity),
+      product_metadata: { year, brand, number, value },
+    });
+  };
 
-    // Send the form data to our forms API on Vercel and get a response.
-    const response = await fetch(endpoint, options);
- 
-    // Get the response data from server as JSON.
-    // If server returns the name submitted, that means the form works.
-    const result = await response.json();
+  const handleCheckout = async () => {
+    setLoading(true);
+    setError(null);
 
-    if (data.email && data.phone) {
-      sendGTMEvent({ event: 'contact_form_lead', value: 'form_submit' })
+    try {
+      const customerRes = await fetch(
+        `/api/stripe/customer?email=${user.email}`
+      );
+      const customerData = await customerRes.json();
+
+      const response = await fetch("/api/stripe/product", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ cartDetails, customerId: customerData.id }),
+      });
+
+      const data = await response.json();
+      if (data.sessionId) {
+        const stripe = await loadStripe(
+          process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+        );
+        clearCart();
+        const { error } = await stripe.redirectToCheckout({
+          sessionId: data.sessionId,
+        });
+        if (error) {
+          setError(error.message);
+        }
+      } else {
+        setError("Failed to create Stripe Checkout session");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError(error.message);
     }
-        
-  }
-  
-  console.log('SubmitCardForm')
-  console.log(data)
+    setLoading(false);
+  };
 
   return (
+    <Grid templateColumns="repeat(4, 1fr)">
+      <GridItem
+        colSpan={2}
+        maxW="680px"
+        mt="24"
+        mb="16"
+        mx="2"
+        bg="neutral.95"
+        p="10"
+        pb="16"
+        borderRadius="20"
+      >
+        <Box mb="8">
+          <TitleLarge color="neutral.10">
+            {data?.attributes?.Form_Heading}
+          </TitleLarge>
+          <BodyMedium color="neutral.10">
+            {data?.attributes?.Form_Subheading}
+          </BodyMedium>
+        </Box>
 
-    <Box maxW='680px' mt='24' mb='16' mx='2' bg='neutral.95' p='10' pb='16' borderRadius='20'>
-
-      <Box mb='8'>
-        <TitleLarge color='neutral.10'>{data?.attributes?.Form_Heading}</TitleLarge>
-        <BodyMedium color='neutral.10'>{data?.attributes?.Form_Subheading}</BodyMedium>
-      </Box>
-
-      <form onSubmit={handleSubmit}>
         <Box>
-
-        <InputFloat label="Player Name" id={"playername"} type={"text"} pattern={"^[A-Za-z]+$"} required={true} />
-          <Box display='flex' flex='wrap' alignItems='center' flexDirection={{ base: 'column', md: 'row' }} required={true}>
-            <Box mb='4' mr='4' minW='200px' h='16'>
-              <Select placeholder='Card Brand' bg='neutral.100' fontSize='1.2rem' h='16' borderRadius='8'>
-                <option value='option1'>Topps</option>
-                <option value='option2'>Panini</option>
-                <option value='option3'>Upper Deck</option>
-                <option value='option3'>Bowman</option>
-                <option value='option3'>Fleer</option>
-                <option value='option3'>Donruss</option>
-                <option value='option3'>Score</option>
-                <option value='option3'>Other - conditional field</option>
+          <Box mr="4" minW="160px">
+            <InputFloat
+              label="Player Name"
+              id={"playername"}
+              type={"text"}
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required={true}
+            />
+          </Box>
+          <Box
+            display="flex"
+            flex="wrap"
+            alignItems="center"
+            flexDirection={{ base: "column", md: "row" }}
+            required={true}
+          >
+            <Box mb="4" mr="4" minW="200px" h="16">
+              <Select
+                placeholder="Card Brand"
+                bg="neutral.100"
+                fontSize="1.2rem"
+                h="16"
+                borderRadius="8"
+                value={brand}
+                onChange={(e) => setBrand(e.target.value)}
+              >
+                <option value="Topps">Topps</option>
+                <option value="Panini">Panini</option>
+                <option value="Upper Deck">Upper Deck</option>
+                <option value="Bowman">Bowman</option>
+                <option value="Fleer">Fleer</option>
+                <option value="Donruss">Donruss</option>
+                <option value="Score">Score</option>
+                <option value="Other">Other</option>
               </Select>
             </Box>
-       
-            <Box mr='4' minW='160px'>
-              <InputFloat label="Card Year" id={"cardyear"} type={"text"} pattern={"^[A-Za-z]+$"} required={true} />
+
+            <Box mr="4" minW="130px">
+              <InputFloat
+                label="Card Year"
+                id={"cardyear"}
+                type={"text"}
+                value={year}
+                onChange={(e) => setYear(e.target.value)}
+                required={true}
+              />
             </Box>
-            <Box mr='4' minW='160px'>
-              <InputFloat label="Card Number" id={"cardnumber"} type={"text"} pattern={"^[A-Za-z]+$"} required={true} />
+            <Box mr="4" minW="130px">
+              <InputFloat
+                label="Card Number"
+                id={"cardnumber"}
+                type={"text"}
+                value={number}
+                onChange={(e) => setNumber(e.target.value)}
+                required={true}
+              />
             </Box>
-          
           </Box>
 
-          <Box mr='4' minW='160px'>
-            <InputFloat label="Description/Parallel/Variation" id={"cardnumber"} type={"text"} pattern={"^[A-Za-z]+$"} required={true} />
+          <Box mr="4" minW="160px">
+            <InputFloat
+              label="Description/Parallel/Variation"
+              id={"description"}
+              type={"text"}
+              value={desc}
+              onChange={(e) => setDesc(e.target.value)}
+              required={true}
+            />
           </Box>
 
-          <Box mr='4' minW='160px'>
-            <InputFloat label="Declared Value" id={"declaredValue"} type={"text"} pattern={"^[A-Za-z]+$"} required={true} />
+          <Box mr="4" minW="160px">
+            <InputFloat
+              label="Declared Value"
+              id={"declaredValue"}
+              type={"text"}
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              required={true}
+            />
           </Box>
 
-          <Box mr='4' maxW='320px'>
-            <InputFloat label="Quantity" id={"quantity"} type={"text"} pattern={"^[A-Za-z]+$"} required={true} />
+          <Box mr="4" maxW="320px">
+            <InputFloat
+              label="Quantity"
+              id={"quantity"}
+              type={"text"}
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+              required={true}
+            />
           </Box>
-
-
-
-
         </Box>
-        
-        <Button 
-          mt="8" 
-          size={{ base: 'md', md: 'lg' }} 
-          // variant="primaryLight" 
-          type="submit" 
-          py='7'
-          bg='primary.40'
-          color='neutral.100'
-          borderRadius='64'
+
+        <Button
+          mt="8"
+          size={{ base: "md", md: "lg" }}
+          // variant="primaryLight"
+          type="submit"
+          py="7"
+          bg="primary.40"
+          color="neutral.100"
+          borderRadius="64"
           _hover={{
-            bg: 'neutral.20',
+            bg: "neutral.20",
           }}
+          onClick={(e) => addToCart()}
         >
-          {'Add to Order'}
+          {"Add to Order"}
         </Button>
-      
-      </form>
-     
-    </Box>
+      </GridItem>
+      <GridItem
+        colSpan={2}
+        maxW="680px"
+        mt="24"
+        mb="16"
+        mx="2"
+        bg="neutral.95"
+        p="10"
+        pb="16"
+        borderRadius="20"
+      >
+        <Box mb="8">
+          <TitleLarge color="neutral.10">Order Summary</TitleLarge>
+          <Box mb="5" mx="2" p="7" bg="neutral.90" borderRadius="20">
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <b>Total Items:</b>
+              <b>{cartCount}</b>
+            </Box>
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <b>Grading Fees:</b>
+              <b>{formattedTotalPrice}</b>
+            </Box>
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <b>Shipping & Insurance:</b>
+              <b>{cartCount}</b>
+            </Box>
+            <Box display={"flex"} justifyContent={"space-between"}>
+              <b>Order Total:</b>
+              <b>{formattedTotalPrice}</b>
+            </Box>
+          </Box>
+          <Button
+            size={{ base: "md", md: "lg" }}
+            // variant="primaryLight"
+            type="submit"
+            py="7"
+            bg="primary.40"
+            color="neutral.100"
+            borderRadius="64"
+            _hover={{
+              bg: "neutral.20",
+            }}
+            onClick={(e) => handleCheckout()}
+          >
+            {"Place Your Order"}
+          </Button>
+        </Box>
 
-  )
-
+        <Box mb="8">
+          <TitleLarge color="neutral.10">Items In Order</TitleLarge>
+          <Box mb="5" mx="2" p="7" bg="neutral.90" borderRadius="20">
+            {Object.keys(cartDetails).length ? (
+              <>
+                {Object.keys(cartDetails).map((item, index) => (
+                  <Box
+                    key={index}
+                    bg="white"
+                    p={3}
+                    mb={2}
+                    borderRadius="md"
+                    shadow="md"
+                  >
+                    <Box display={"flex"} justifyContent={"space-between"}>
+                      <b>{`${cartDetails[item].product_data.year} ${cartDetails[item].product_data.brand} ${cartDetails[item].name} #${cartDetails[item].product_data.number}`}</b>
+                      <b>Quantity: {cartDetails[item].quantity}</b>
+                    </Box>
+                    <Box display={"flex"} justifyContent={"space-between"}>
+                      <b>
+                        Declared Value: ${cartDetails[item].product_data.value}
+                      </b>
+                    </Box>
+                    <Box display={"flex"} justifyContent={"space-between"}>
+                      <b>Grading Fee:</b>
+                      <b>{cartDetails[item].formattedValue}</b>
+                    </Box>
+                    <Box display={"flex"} justifyContent={"space-between"}>
+                      <b>Shipping & Insurance:</b>
+                      <b>{cartDetails[item].formattedValue}</b>
+                    </Box>
+                    <Box display={"flex"} justifyContent={"space-between"}>
+                      <b>Total:</b>
+                      <b>{cartDetails[item].formattedValue}</b>
+                    </Box>
+                  </Box>
+                ))}
+              </>
+            ) : (
+              <Text>No Items</Text>
+            )}
+          </Box>
+          <Button
+            size={{ base: "md", md: "lg" }}
+            // variant="primaryLight"
+            type="submit"
+            py="7"
+            bg="primary.40"
+            color="neutral.100"
+            borderRadius="64"
+            _hover={{
+              bg: "neutral.20",
+            }}
+            onClick={(e) => clearCart()}
+          >
+            {"Clear the Cart"}
+          </Button>
+        </Box>
+      </GridItem>
+    </Grid>
+  );
 }
-
--- src/app/_layout/hero-header/Header.js
+++ src/app/_layout/hero-header/Header.js
@@ -1,50 +1,51 @@
-'use client'
-import { Box, Image, Link, Text } from "@chakra-ui/react";
+"use client";
+import { Box, Image, Link, Text, Button } from "@chakra-ui/react";
 import Desktop from "./navigation/Desktop";
 import FullContainer from "../containers/FullContainer";
+import { useUser } from '@auth0/nextjs-auth0/client';
 
-
-export default function Header({ colorScheme }) {
-
-  colorScheme = colorScheme ? colorScheme : 'neutralDark';
+export default function Header({ data, children, contentType }) {
+  const { user, error, isLoading } = useUser();
 
   return (
-
     <FullContainer>
-
       <Box
-        display='flex'
-        alignItems='center'
-        justifyContent='space-between'
-        py='6'
-      >    
-        <Box
-          display='flex'
-          alignItems='center'
-          flexGrow='2'
-        >
-          <Box mr='12' maxW='260px'>
-            <Link href='/' variant='noDeco'>
-            <Image 
-              src="/logo-gold.png"
-              fill
-              alt=""
-            />
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        py="6"
+      >
+        <Box display="flex" alignItems="center" flexGrow="2">
+          <Box mr="12" maxW="260px">
+            <Link href="/" variant="noDeco">
+              <Image src="/logo-gold.png" fill alt="" />
             </Link>
           </Box>
-          
         </Box>
 
-        <Box display='flex' alignItems='center'>
-          <Box mr='16' display={{ base: 'none', xl: 'block' }}>
+        <Box display="flex" gap={5} alignItems="center">
+          <Box mr="16">
             <Desktop />
           </Box>
-          <Link href='#' variant='neutralDark'>Sign Up Today</Link>
+          {!user ? (
+            <Link href="/api/auth/login" variant="neutralDark">
+              Sign Up Today
+            </Link>
+          ) : (
+            <>
+              <Link href="/form/submit-card" variant="neutralDark">
+                Submit a Card
+              </Link>
+              <Link href="/account" variant="neutralDark">
+                My Account
+              </Link>
+              <Link href="/api/auth/logout" variant="neutralDark">
+                Logout
+              </Link>
+            </>
+          )}
         </Box>
-
       </Box>
-
     </FullContainer>
-
-  )
+  );
 }
