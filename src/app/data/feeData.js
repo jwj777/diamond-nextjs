eef80498564a23627fdc4017d2d1c093ef4face5
Webster <webster.jorgensen@gmail.com>
2024-06-08 02:14:41 -0500

    fee data remove '.0'

--- src/app/data/feeData.js
+++ src/app/data/feeData.js
@@ -1,606 +1,606 @@
 
 export const fees = {
-  "1.0": {
+  "1": {
     "Standard": 20.0,
     "Diamond Club": 18.0,
     "Diamond Premium": 16.0,
     "Dealers Club": 14.0
   },
-  "100.0": {
+  "100": {
     "Standard": 20.0,
     "Diamond Club": 18.0,
     "Diamond Premium": 16.0,
     "Dealers Club": 14.0
   },
-  "200.0": {
+  "200": {
     "Standard": 20.0,
     "Diamond Club": 18.0,
     "Diamond Premium": 16.0,
     "Dealers Club": 14.0
   },
-  "300.0": {
+  "300": {
     "Standard": 20.0,
     "Diamond Club": 18.0,
     "Diamond Premium": 16.0,
     "Dealers Club": 14.0
   },
-  "400.0": {
+  "400": {
     "Standard": 20.0,
     "Diamond Club": 18.0,
     "Diamond Premium": 16.0,
     "Dealers Club": 14.0
   },
-  "500.0": {
+  "500": {
     "Standard": 20.0,
     "Diamond Club": 18.0,
     "Diamond Premium": 16.0,
     "Dealers Club": 14.0
   },
-  "600.0": {
+  "600": {
     "Standard": 22.0,
     "Diamond Club": 19.8,
     "Diamond Premium": 17.6,
     "Dealers Club": 15.4
   },
-  "700.0": {
+  "700": {
     "Standard": 24.0,
     "Diamond Club": 21.6,
     "Diamond Premium": 19.2,
     "Dealers Club": 16.8
   },
-  "800.0": {
+  "800": {
     "Standard": 26.0,
     "Diamond Club": 23.4,
     "Diamond Premium": 20.8,
     "Dealers Club": 18.2
   },
-  "900.0": {
+  "900": {
     "Standard": 28.0,
     "Diamond Club": 25.2,
     "Diamond Premium": 22.4,
     "Dealers Club": 19.6
   },
-  "1000.0": {
+  "1000": {
     "Standard": 30.0,
     "Diamond Club": 27.0,
     "Diamond Premium": 24.0,
     "Dealers Club": 21.0
   },
-  "1100.0": {
+  "1100": {
     "Standard": 32.0,
     "Diamond Club": 28.8,
     "Diamond Premium": 25.6,
     "Dealers Club": 22.4
   },
-  "1200.0": {
+  "1200": {
     "Standard": 34.0,
     "Diamond Club": 30.6,
     "Diamond Premium": 27.2,
     "Dealers Club": 23.8
   },
-  "1300.0": {
+  "1300": {
     "Standard": 36.0,
     "Diamond Club": 32.4,
     "Diamond Premium": 28.8,
     "Dealers Club": 25.2
   },
-  "1400.0": {
+  "1400": {
     "Standard": 38.0,
     "Diamond Club": 34.2,
     "Diamond Premium": 30.4,
     "Dealers Club": 26.6
   },
-  "1500.0": {
+  "1500": {
     "Standard": 40.0,
     "Diamond Club": 36.0,
     "Diamond Premium": 32.0,
     "Dealers Club": 28.0
   },
-  "1600.0": {
+  "1600": {
     "Standard": 44.0,
     "Diamond Club": 39.6,
     "Diamond Premium": 35.2,
     "Dealers Club": 30.8
   },
-  "1700.0": {
+  "1700": {
     "Standard": 48.0,
     "Diamond Club": 43.2,
     "Diamond Premium": 38.4,
     "Dealers Club": 33.6
   },
-  "1800.0": {
+  "1800": {
     "Standard": 52.0,
     "Diamond Club": 46.8,
     "Diamond Premium": 41.6,
     "Dealers Club": 36.4
   },
-  "1900.0": {
+  "1900": {
     "Standard": 56.0,
     "Diamond Club": 50.4,
     "Diamond Premium": 44.8,
     "Dealers Club": 39.2
   },
-  "2000.0": {
+  "2000": {
     "Standard": 60.0,
     "Diamond Club": 54.0,
     "Diamond Premium": 48.0,
     "Dealers Club": 42.0
   },
-  "2100.0": {
+  "2100": {
     "Standard": 64.0,
     "Diamond Club": 57.6,
     "Diamond Premium": 51.2,
     "Dealers Club": 44.8
   },
-  "2200.0": {
+  "2200": {
     "Standard": 68.0,
     "Diamond Club": 61.2,
     "Diamond Premium": 54.4,
     "Dealers Club": 47.6
   },
-  "2300.0": {
+  "2300": {
     "Standard": 72.0,
     "Diamond Club": 64.8,
     "Diamond Premium": 57.6,
     "Dealers Club": 50.4
   },
-  "2400.0": {
+  "2400": {
     "Standard": 76.0,
     "Diamond Club": 68.4,
     "Diamond Premium": 60.8,
     "Dealers Club": 53.2
   },
-  "2500.0": {
+  "2500": {
     "Standard": 80.0,
     "Diamond Club": 72.0,
     "Diamond Premium": 64.0,
     "Dealers Club": 56.0
   },
-  "2600.0": {
+  "2600": {
     "Standard": 83.2,
     "Diamond Club": 74.88,
     "Diamond Premium": 66.56,
     "Dealers Club": 58.24
   },
-  "2700.0": {
+  "2700": {
     "Standard": 86.4,
     "Diamond Club": 77.76,
     "Diamond Premium": 69.12,
     "Dealers Club": 60.48
   },
-  "2800.0": {
+  "2800": {
     "Standard": 89.6,
     "Diamond Club": 80.64,
     "Diamond Premium": 71.68,
     "Dealers Club": 62.72
   },
-  "2900.0": {
+  "2900": {
     "Standard": 92.8,
     "Diamond Club": 83.52,
     "Diamond Premium": 74.24,
     "Dealers Club": 64.96
   },
-  "3000.0": {
+  "3000": {
     "Standard": 96.0,
     "Diamond Club": 86.4,
     "Diamond Premium": 76.8,
     "Dealers Club": 67.2
   },
-  "3100.0": {
+  "3100": {
     "Standard": 99.2,
     "Diamond Club": 89.28,
     "Diamond Premium": 79.36,
     "Dealers Club": 69.44
   },
-  "3200.0": {
+  "3200": {
     "Standard": 102.4,
     "Diamond Club": 92.16,
     "Diamond Premium": 81.92,
     "Dealers Club": 71.68
   },
-  "3300.0": {
+  "3300": {
     "Standard": 105.6,
     "Diamond Club": 95.04,
     "Diamond Premium": 84.48,
     "Dealers Club": 73.92
   },
-  "3400.0": {
+  "3400": {
     "Standard": 108.8,
     "Diamond Club": 97.92,
     "Diamond Premium": 87.04,
     "Dealers Club": 76.16
   },
-  "3500.0": {
+  "3500": {
     "Standard": 112.0,
     "Diamond Club": 100.8,
     "Diamond Premium": 89.6,
     "Dealers Club": 78.4
   },
-  "3600.0": {
+  "3600": {
     "Standard": 115.2,
     "Diamond Club": 103.68,
     "Diamond Premium": 92.16,
     "Dealers Club": 80.64
   },
-  "3700.0": {
+  "3700": {
     "Standard": 118.4,
     "Diamond Club": 106.56,
     "Diamond Premium": 94.72,
     "Dealers Club": 82.88
   },
-  "3800.0": {
+  "3800": {
     "Standard": 121.6,
     "Diamond Club": 109.44,
     "Diamond Premium": 97.28,
     "Dealers Club": 85.12
   },
-  "3900.0": {
+  "3900": {
     "Standard": 124.8,
     "Diamond Club": 112.32,
     "Diamond Premium": 99.84,
     "Dealers Club": 87.36
   },
-  "4000.0": {
+  "4000": {
     "Standard": 128.0,
     "Diamond Club": 115.2,
     "Diamond Premium": 102.4,
     "Dealers Club": 89.6
   },
-  "4100.0": {
+  "4100": {
     "Standard": 131.2,
     "Diamond Club": 118.08,
     "Diamond Premium": 104.96,
     "Dealers Club": 91.84
   },
-  "4200.0": {
+  "4200": {
     "Standard": 134.4,
     "Diamond Club": 120.96,
     "Diamond Premium": 107.52,
     "Dealers Club": 94.08
   },
-  "4300.0": {
+  "4300": {
     "Standard": 137.6,
     "Diamond Club": 123.84,
     "Diamond Premium": 110.08,
     "Dealers Club": 96.32
   },
-  "4400.0": {
+  "4400": {
     "Standard": 140.8,
     "Diamond Club": 126.72,
     "Diamond Premium": 112.64,
     "Dealers Club": 98.56
   },
-  "4500.0": {
+  "4500": {
     "Standard": 144.0,
     "Diamond Club": 129.6,
     "Diamond Premium": 115.2,
     "Dealers Club": 100.8
   },
-  "4600.0": {
+  "4600": {
     "Standard": 147.2,
     "Diamond Club": 132.48,
     "Diamond Premium": 117.76,
     "Dealers Club": 103.04
   },
-  "4700.0": {
+  "4700": {
     "Standard": 150.4,
     "Diamond Club": 135.36,
     "Diamond Premium": 120.32,
     "Dealers Club": 105.28
   },
-  "4800.0": {
+  "4800": {
     "Standard": 153.6,
     "Diamond Club": 138.24,
     "Diamond Premium": 122.88,
     "Dealers Club": 107.52
   },
-  "4900.0": {
+  "4900": {
     "Standard": 156.8,
     "Diamond Club": 141.12,
     "Diamond Premium": 125.44,
     "Dealers Club": 109.76
   },
-  "5000.0": {
+  "5000": {
     "Standard": 160.0,
     "Diamond Club": 144.0,
     "Diamond Premium": 128.0,
     "Dealers Club": 112.0
   },
-  "5100.0": {
+  "5100": {
     "Standard": 163.2,
     "Diamond Club": 146.96,
     "Diamond Premium": 130.56,
     "Dealers Club": 114.24
   },
-  "5200.0": {
+  "5200": {
     "Standard": 166.4,
     "Diamond Club": 149.92,
     "Diamond Premium": 133.12,
     "Dealers Club": 116.48
   },
-  "5300.0": {
+  "5300": {
     "Standard": 169.6,
     "Diamond Club": 152.88,
     "Diamond Premium": 135.68,
     "Dealers Club": 118.72
   },
-  "5400.0": {
+  "5400": {
     "Standard": 172.8,
     "Diamond Club": 155.84,
     "Diamond Premium": 138.24,
     "Dealers Club": 120.96
   },
-  "5500.0": {
+  "5500": {
     "Standard": 176.0,
     "Diamond Club": 158.8,
     "Diamond Premium": 140.8,
     "Dealers Club": 123.2
   },
-  "5600.0": {
+  "5600": {
     "Standard": 179.2,
     "Diamond Club": 161.76,
     "Diamond Premium": 143.36,
     "Dealers Club": 125.44
   },
-  "5700.0": {
+  "5700": {
     "Standard": 182.4,
     "Diamond Club": 164.72,
     "Diamond Premium": 145.92,
     "Dealers Club": 127.68
   },
-  "5800.0": {
+  "5800": {
     "Standard": 185.6,
     "Diamond Club": 167.68,
     "Diamond Premium": 148.48,
     "Dealers Club": 129.92
   },
-  "5900.0": {
+  "5900": {
     "Standard": 188.8,
     "Diamond Club": 170.64,
     "Diamond Premium": 151.04,
     "Dealers Club": 132.16
   },
-  "6000.0": {
+  "6000": {
     "Standard": 192.0,
     "Diamond Club": 173.6,
     "Diamond Premium": 153.6,
     "Dealers Club": 134.4
   },
-  "6100.0": {
+  "6100": {
     "Standard": 195.2,
     "Diamond Club": 176.56,
     "Diamond Premium": 156.16,
     "Dealers Club": 136.64
   },
-  "6200.0": {
+  "6200": {
     "Standard": 198.4,
     "Diamond Club": 179.52,
     "Diamond Premium": 158.72,
     "Dealers Club": 138.88
   },
-  "6300.0": {
+  "6300": {
     "Standard": 201.6,
     "Diamond Club": 182.48,
     "Diamond Premium": 161.28,
     "Dealers Club": 141.12
   },
-  "6400.0": {
+  "6400": {
     "Standard": 204.8,
     "Diamond Club": 185.44,
     "Diamond Premium": 163.84,
     "Dealers Club": 143.36
   },
-  "6500.0": {
+  "6500": {
     "Standard": 208.0,
     "Diamond Club": 188.4,
     "Diamond Premium": 166.4,
     "Dealers Club": 145.6
   },
-  "6600.0": {
+  "6600": {
     "Standard": 211.2,
     "Diamond Club": 191.36,
     "Diamond Premium": 168.96,
     "Dealers Club": 147.84
   },
-  "6700.0": {
+  "6700": {
     "Standard": 214.4,
     "Diamond Club": 194.32,
     "Diamond Premium": 171.52,
     "Dealers Club": 150.08
   },
-  "6800.0": {
+  "6800": {
     "Standard": 217.6,
     "Diamond Club": 197.28,
     "Diamond Premium": 174.08,
     "Dealers Club": 152.32
   },
-  "6900.0": {
+  "6900": {
     "Standard": 220.8,
     "Diamond Club": 200.24,
     "Diamond Premium": 176.64,
     "Dealers Club": 154.56
   },
-  "7000.0": {
+  "7000": {
     "Standard": 224.0,
     "Diamond Club": 203.2,
     "Diamond Premium": 179.2,
     "Dealers Club": 156.8
   },
-  "7100.0": {
+  "7100": {
     "Standard": 227.2,
     "Diamond Club": 206.16,
     "Diamond Premium": 181.76,
     "Dealers Club": 159.04
   },
-  "7200.0": {
+  "7200": {
     "Standard": 230.4,
     "Diamond Club": 209.12,
     "Diamond Premium": 184.32,
     "Dealers Club": 161.28
   },
-  "7300.0": {
+  "7300": {
     "Standard": 233.6,
     "Diamond Club": 212.08,
     "Diamond Premium": 186.88,
     "Dealers Club": 163.52
   },
-  "7400.0": {
+  "7400": {
     "Standard": 236.8,
     "Diamond Club": 215.04,
     "Diamond Premium": 189.44,
     "Dealers Club": 165.76
   },
-  "7500.0": {
+  "7500": {
     "Standard": 240.0,
     "Diamond Club": 218.0,
     "Diamond Premium": 192.0,
     "Dealers Club": 168.0
   },
-  "7600.0": {
+  "7600": {
     "Standard": 243.2,
     "Diamond Club": 220.8,
     "Diamond Premium": 194.56,
     "Dealers Club": 170.24
   },
-  "7700.0": {
+  "7700": {
     "Standard": 246.4,
     "Diamond Club": 223.6,
     "Diamond Premium": 197.12,
     "Dealers Club": 172.48
   },
-  "7800.0": {
+  "7800": {
     "Standard": 249.6,
     "Diamond Club": 226.4,
     "Diamond Premium": 199.68,
     "Dealers Club": 174.72
   },
-  "7900.0": {
+  "7900": {
     "Standard": 252.8,
     "Diamond Club": 229.2,
     "Diamond Premium": 202.24,
     "Dealers Club": 176.96
   },
-  "8000.0": {
+  "8000": {
     "Standard": 256.0,
     "Diamond Club": 232.0,
     "Diamond Premium": 204.8,
     "Dealers Club": 179.2
   },
-  "8100.0": {
+  "8100": {
     "Standard": 259.2,
     "Diamond Club": 234.8,
     "Diamond Premium": 207.36,
     "Dealers Club": 181.44
   },
-  "8200.0": {
+  "8200": {
     "Standard": 262.4,
     "Diamond Club": 237.6,
     "Diamond Premium": 209.92,
     "Dealers Club": 183.68
   },
-  "8300.0": {
+  "8300": {
     "Standard": 265.6,
     "Diamond Club": 240.4,
     "Diamond Premium": 212.48,
     "Dealers Club": 185.92
   },
-  "8400.0": {
+  "8400": {
     "Standard": 268.8,
     "Diamond Club": 243.2,
     "Diamond Premium": 215.04,
     "Dealers Club": 188.16
   },
-  "8500.0": {
+  "8500": {
     "Standard": 272.0,
     "Diamond Club": 246.0,
     "Diamond Premium": 217.6,
     "Dealers Club": 190.4
   },
-  "8600.0": {
+  "8600": {
     "Standard": 275.2,
     "Diamond Club": 248.8,
     "Diamond Premium": 220.16,
     "Dealers Club": 192.64
   },
-  "8700.0": {
+  "8700": {
     "Standard": 278.4,
     "Diamond Club": 251.6,
     "Diamond Premium": 222.72,
     "Dealers Club": 194.88
   },
-  "8800.0": {
+  "8800": {
     "Standard": 281.6,
     "Diamond Club": 254.4,
     "Diamond Premium": 225.28,
     "Dealers Club": 197.12
   },
-  "8900.0": {
+  "8900": {
     "Standard": 284.8,
     "Diamond Club": 257.2,
     "Diamond Premium": 227.84,
     "Dealers Club": 199.36
   },
-  "9000.0": {
+  "9000": {
     "Standard": 288.0,
     "Diamond Club": 260.0,
     "Diamond Premium": 230.4,
     "Dealers Club": 201.6
   },
-  "9100.0": {
+  "9100": {
     "Standard": 291.2,
     "Diamond Club": 262.8,
     "Diamond Premium": 232.96,
     "Dealers Club": 203.84
   },
-  "9200.0": {
+  "9200": {
     "Standard": 294.4,
     "Diamond Club": 265.6,
     "Diamond Premium": 235.52,
     "Dealers Club": 206.08
   },
-  "9300.0": {
+  "9300": {
     "Standard": 297.6,
     "Diamond Club": 268.4,
     "Diamond Premium": 238.08,
     "Dealers Club": 208.32
   },
-  "9400.0": {
+  "9400": {
     "Standard": 300.8,
     "Diamond Club": 271.2,
     "Diamond Premium": 240.64,
     "Dealers Club": 210.56
   },
-  "9500.0": {
+  "9500": {
     "Standard": 304.0,
     "Diamond Club": 274.0,
     "Diamond Premium": 243.2,
     "Dealers Club": 212.8
   },
-  "9600.0": {
+  "9600": {
     "Standard": 307.2,
     "Diamond Club": 276.8,
     "Diamond Premium": 245.76,
     "Dealers Club": 215.04
   },
-  "9700.0": {
+  "9700": {
     "Standard": 310.4,
     "Diamond Club": 279.6,
     "Diamond Premium": 248.32,
     "Dealers Club": 217.28
   },
-  "9800.0": {
+  "9800": {
     "Standard": 313.6,
     "Diamond Club": 282.4,
     "Diamond Premium": 250.88,
     "Dealers Club": 219.52
   },
-  "9900.0": {
+  "9900": {
     "Standard": 316.8,
     "Diamond Club": 285.2,
     "Diamond Premium": 253.44,
     "Dealers Club": 221.76
   },
-  "10000.0": {
+  "10000": {
     "Standard": 320.0,
     "Diamond Club": 288.0,
     "Diamond Premium": 256.0,
