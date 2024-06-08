972b042069c928b125c660496cfd63a94d624b17
Webster <webster.jorgensen@gmail.com>
2024-06-07 23:57:46 -0500

    my debug test 2

--- src/app/_components/form/SubmitCardForm.js
+++ src/app/_components/form/SubmitCardForm.js
@@ -93,6 +93,7 @@ export default function SubmitCardForm({ data }) {
     console.log("Selected Level (string):", levelString);
     // console.log("Fees at Selected Level:", JSON.stringify(fees[levelString], null, 2));
 
+    console.log("My Test level --> ", level)
     console.log("My Test --> ", fees[level])
   
     // Check if subscription level exists at the selected level
