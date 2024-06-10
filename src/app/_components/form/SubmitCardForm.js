4f99cba230487f1ed5c181e9450914fb49d99ed2
Webster <webster.jorgensen@gmail.com>
2024-06-09 19:29:48 -0500

    insurance data format

--- src/app/_components/form/SubmitCardForm.js
+++ src/app/_components/form/SubmitCardForm.js
@@ -124,7 +124,7 @@ export default function SubmitCardForm({ data }) {
   function getInsuranceCost(declaredValue) {
     const levels = Object.keys(insuranceCost).map(parseFloat).sort((a, b) => a - b);
 
-    declaredValue = Math.round(declaredValue) - 100
+    declaredValue = (declaredValue*100)/100 - 100
     
     console.log('declared value ', declaredValue)
     console.log(typeof declaredValue)
