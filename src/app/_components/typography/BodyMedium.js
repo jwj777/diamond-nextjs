9676ed6378fbe5a55bb257c420a52577b440428c
Webster <webster.jorgensen@gmail.com>
2024-06-16 13:51:23 -0500

    font test 2

--- src/app/_components/typography/BodyMedium.js
+++ src/app/_components/typography/BodyMedium.js
@@ -9,10 +9,12 @@ export default function BodyMedium({ text, textColor, color, children, mb, mr })
   color ? color = color : color = 'neutral.10'
 
   return (
-    <Box color={textColor} className={styles.bodyContent2 + ' bodyMedium'}>
+    <Box 
+      color={textColor} 
+      // className={styles.bodyContent2 + ' bodyMedium'}
+    >
 
       <Box 
-        fontFamily='figtree'
         fontSize={{ base: '1rem', md: '1rem' }}
         lineHeight={{ base: '152%', md: '152%' }}
         color={color}
