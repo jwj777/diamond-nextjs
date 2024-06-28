62fc855ea8523b1aac1256c942b7d6a518cd513e
Webster <webster.jorgensen@gmail.com>
2024-06-28 03:52:59 -0500

    pricing grids

---
+++ src/app/_components/sections/PriceGrids.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Box, Table, Thead, Tbody, Tr, Th, Td, Text, Flex } from "@chakra-ui/react";
+import TitleSmall from '../typography/TitleSmall';
+import HeadlineSmall from '../typography/HeadlineSmall';
+import BodyMedium from '../typography/BodyMedium';
+import BodySuper from '../typography/BodySuper';
+
+function PriceGrids({ priceGridStandard, priceGridBulk }) {
+
+  const renderTable = (data, title) => {
+    if (!data) {
+      return <Text>No data available</Text>;
+    }
+
+    const tiers = Object.keys(data);
+    const declaredValues = Object.keys(data[tiers[0]]);
+
+    return (
+     <Box mb={8} bg='neutral.10' p='10' borderRadius='1.5rem'>
+      <BodySuper color='neutral.95'>{title}</BodySuper>
+
+      <Flex direction="column" mt='8'>
+
+        <Flex borderBottom='1px' borderColor='neutral.30' pb='2'>
+          <Box flex="1" p={2} fontWeight="bold" >
+            <TitleSmall color='primary.90'>Declared Value</TitleSmall>
+          </Box>
+          {tiers.map((tier) => (
+            <Box key={tier} flex="1" p={2} textAlign={'center'}>
+              <BodyMedium color='primary.90'>{tier}</BodyMedium>
+            </Box>
+          ))}
+        </Flex>
+
+        {declaredValues.map((value) => (
+          <Flex key={value} borderBottom="1px" borderColor='neutral.30' py='1'>
+            <Box flex="1" p={2}>
+            <BodyMedium color='neutral.98'>{'$' + value}</BodyMedium>
+            </Box>
+            {tiers.map((tier) => (
+              <Box key={tier} flex="1" p={2} textAlign={'center'}>
+                <BodyMedium color='neutral.80'>{'$' + data[tier][value]}</BodyMedium>
+              </Box>
+            ))}
+          </Flex>
+        ))}
+
+      </Flex>
+
+    </Box>
+    );
+  };
+
+  return (
+    <Box>
+      {renderTable(priceGridStandard, "Standard Pricing")}
+      {renderTable(priceGridBulk, "Bulk Pricing")}
+    </Box>
+  );
+}
+
+export default PriceGrids;
--- src/app/page/pricing/page.js
+++ src/app/page/pricing/page.js
@@ -1,17 +1,26 @@
 import PricingCards from "@/app/_components/sections/PricingCards";
-import GradeAccordion from "@/app/_components/sections/grading-standards/GradeAccordion";
-import MemberPlans from "@/app/_components/sections/member-plans/MemberPlans";
-import Sections from "@/app/_components/sections/sections-array/Sections";
 import PageContainerGeneral from "@/app/_layout/PageContainerGeneral";
 import XlContainer from "@/app/_layout/containers/XlContainer";
 import { Box, Image } from "@chakra-ui/react";
+import { priceGridStandard, priceGridBulk } from "@/app/data/priceGrids";
+import PriceGrids from "@/app/_components/sections/PriceGrids";
+
 
 export default async function Page({ params }) {
+
+
+  console.log("Page component is mounting"); 
+
+  // Log the imported data to verify
+  console.log("priceGridStandard:", priceGridStandard);
+  console.log("priceGridBulk:", priceGridBulk);
 
   const pageBySlug = await getPageBySlug(params);
   const data = pageBySlug?.[0];
 
-  const plansData = await getPlans(params);
+
+  // const plansData = await getPlans(params);
+
 
   return (
  
@@ -22,6 +31,11 @@ export default async function Page({ params }) {
           <XlContainer>
 
             <PricingCards />
+
+
+            <Box mb='24'>
+              <PriceGrids priceGridStandard={priceGridStandard} priceGridBulk={priceGridBulk} />
+            </Box>
 
         </XlContainer>
       </Box>
@@ -34,6 +48,7 @@ export default async function Page({ params }) {
 }
 
 async function getPageBySlug(params) {
+  console.log("Fetching page by slug:", params.slug);
   try {
     const slug = params.slug;
     const response =
