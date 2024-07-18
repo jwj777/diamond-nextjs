b1b2cbdc6d40ba933b0565a7e0290aa8bdc30a5c
Webster <webster.jorgensen@gmail.com>
2024-07-17 21:05:53 -0500

    my account loading spinner

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -20,6 +20,7 @@ export default function ProfileClient() {
     if (!isLoading && !user) {
       return redirect("/");
     }
+    if (!user) return;
     setLoading(true);
     async function fetchSubscriptions() {
       try {
@@ -33,7 +34,6 @@ export default function ProfileClient() {
         );
         const data = await response.json();
         setSubscriptions(data);
-        console.log("subsc", data);
         setLoading(false);
       } catch (error) {
         setLoading(false);
@@ -46,93 +46,96 @@ export default function ProfileClient() {
     <main>
       <PageContainerAccount data={{ user }}>
         <XlContainer>
-          <Box 
-            display='flex'
-            flexDir={'column'}
-            mt='8'
-            pb='24'
-            position="relative" 
-            bg='neutral.10' 
-          >
-            {isLoading || loading ? (
-              <Spinner size="xl" />
-            ) : (
-              <>
-                <TitleLarge color='neutral.90'>My Account</TitleLarge>
-                <Box 
-                  display='inline-block' 
-                  position="relative" 
-                  mt='8' 
-                  width='fit-content'
-                  bg='neutral.20' 
-                  p='8' 
-                  pr='12'
-                  borderRadius='1rem'
-                >
-                  <Box mb='2'>
-                    <BodyMedium color={"white"}>
-                      <Text as='span' fontWeight='600' mr='1'>Email: </Text>
-                      <Text as='span' color='neutral.90'>{user.email}</Text>
-                    </BodyMedium>
-                  </Box>
-                  <Box>
-                    <BodyMedium color={"white"}>
-                      <Text as='span' fontWeight='600' mr='1'>Username: </Text>
-                      <Text as='span' color='neutral.90'>{user.nickname}</Text>
-                    </BodyMedium>
-                  </Box>
+          {isLoading || loading ? (
+            <Box display="flex" justifyContent="center" pt="24" pb="24">
+              <Box textAlign={'center'}>
+                <Spinner color="primary.80" emptyColor="neutral.30" size="xl" mb="2" />
+                <BodyMedium color="primary.80">Loading...</BodyMedium>
+              </Box>
+            </Box>
+          ) : (
+            <Box 
+              display='flex'
+              flexDir={'column'}
+              mt='8'
+              pb='24'
+              position="relative" 
+              bg='neutral.10' 
+            >
+              <TitleLarge color='neutral.90'>My Account</TitleLarge>
+              <Box 
+                display='inline-block' 
+                position="relative" 
+                mt='8' 
+                width='fit-content'
+                bg='neutral.20' 
+                p='8' 
+                pr='12'
+                borderRadius='1rem'
+              >
+                <Box mb='2'>
+                  <BodyMedium color={"white"}>
+                    <Text as='span' fontWeight='600' mr='1'>Email: </Text>
+                    <Text as='span' color='neutral.90'>{user.email}</Text>
+                  </BodyMedium>
                 </Box>
-                <Box 
-                  display='inline-block' 
-                  position="relative" 
-                  width='fit-content'
-                  mt='8' 
-                  bg='neutral.20' 
-                  p='8' 
-                  pr='12'
-                  borderRadius='1rem'
-                >
-                  <TitleMedium color='neutral.90'>Membership Details</TitleMedium>
-                  {subscriptions.length > 0 ? (
-                    <Box mt='3'>
-                      <Box mb='2'>
-                        <BodyMedium color={"white"}>
-                          <Text as='span' fontWeight='600' mr='1' mb='3'>Membership Plan:</Text>
-                          <Text as='span' color='neutral.90'>{subscriptions[0].product.name}</Text>
-                        </BodyMedium>
-                      </Box>
-                      <Box mb='2'>
-                        <BodyMedium color={"white"}>
-                          <Text as='span' fontWeight='600' mr='1' mb='3'>Yearly Fee: </Text>
-                          <Text as='span' color='neutral.90'>${subscriptions[0].plan.amount / 100}</Text>
-                        </BodyMedium>
-                      </Box>
-                        <Box mb='2'>
-                        <BodyMedium color={"white"}>
-                        <Text as='span' fontWeight='600' mr='1' mb='3'>Status:{" "}</Text>
-                        <Text as='span' color='neutral.90'>{subscriptions[0].plan.active ? "Active" : "Deactive"}</Text>
-                        </BodyMedium>
-                      </Box>
-                      <Box mt='4'>
-                        <Link href='/page/pricing' variant='primaryDarkText' size='mdText'>Upgrade Plan</Link>
-                      </Box>
+                <Box>
+                  <BodyMedium color={"white"}>
+                    <Text as='span' fontWeight='600' mr='1'>Username: </Text>
+                    <Text as='span' color='neutral.90'>{user.nickname}</Text>
+                  </BodyMedium>
+                </Box>
+              </Box>
+              <Box 
+                display='inline-block' 
+                position="relative" 
+                width='fit-content'
+                mt='8' 
+                bg='neutral.20' 
+                p='8' 
+                pr='12'
+                borderRadius='1rem'
+              >
+                <TitleMedium color='neutral.90'>Membership Details</TitleMedium>
+                {subscriptions.length > 0 ? (
+                  <Box mt='3'>
+                    <Box mb='2'>
+                      <BodyMedium color={"white"}>
+                        <Text as='span' fontWeight='600' mr='1' mb='3'>Membership Plan:</Text>
+                        <Text as='span' color='neutral.90'>{subscriptions[0].product.name}</Text>
+                      </BodyMedium>
                     </Box>
-                  
-                  ) : (
+                    <Box mb='2'>
+                      <BodyMedium color={"white"}>
+                        <Text as='span' fontWeight='600' mr='1' mb='3'>Yearly Fee: </Text>
+                        <Text as='span' color='neutral.90'>${subscriptions[0].plan.amount / 100}</Text>
+                      </BodyMedium>
+                    </Box>
+                      <Box mb='2'>
+                      <BodyMedium color={"white"}>
+                      <Text as='span' fontWeight='600' mr='1' mb='3'>Status:{" "}</Text>
+                      <Text as='span' color='neutral.90'>{subscriptions[0].plan.active ? "Active" : "Deactive"}</Text>
+                      </BodyMedium>
+                    </Box>
                     <Box mt='4'>
-                      <BodyMedium color={"white"} mb='2'>No Subscription.</BodyMedium>
-                      <Link href='/page/pricing' variant='primaryDarkText' size='mdText'>Subscribe To a Plan</Link>
+                      <Link href='/page/pricing' variant='primaryDarkText' size='mdText'>Upgrade Plan</Link>
                     </Box>
-                  )}
-                </Box>
-                <Box position="relative" mt='16'>
-                  <Link href="/api/auth/logout" variant="neutralDark">
-                    Logout
-                  </Link>
-                </Box>
-              </>
-            )}
-          </Box>
+                  </Box>
+                
+                ) : (
+                  <Box mt='4'>
+                    <BodyMedium color={"white"} mb='2'>No Subscription.</BodyMedium>
+                    <Link href='/page/pricing' variant='primaryDarkText' size='mdText'>Subscribe To a Plan</Link>
+                  </Box>
+                )}
+              </Box>
+              <Box position="relative" mt='16'>
+                <Link href="/api/auth/logout" variant="neutralDark">
+                  Logout
+                </Link>
+              </Box>
+            </Box>
+          )}
         </XlContainer>
         <Footer />
       </PageContainerAccount>
