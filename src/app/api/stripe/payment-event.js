d6e8587b92666a17646ec7d9d5ea90febc06a841
Webster <webster.jorgensen@gmail.com>
2024-10-16 17:00:27 -0500

    new membership email route

--- src/app/api/stripe/payment-event.js
+++ src/app/api/stripe/payment-event.js
@@ -38,45 +38,15 @@ export default async (req, res) => {
       const { line_items } = sessionWithLineItems;
 
       // Send the cartDetails to Zapier Webhook
-      // await fetch("https://hooks.zapier.com/hooks/catch/8026392/2b1epr8/", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify({ line_items }),
-      // });
-
-      // console.log("Sent line_items to Zapier:", JSON.stringify(line_items, null, 2));
-
-      const cardGradingItems = line_items.data.filter(item => {
-        // Assuming card grading products have specific product IDs or names you can check against
-        return item.description.includes("Card Grading") || item.price.product === "prod_QXnI7So14My2dN";
+      await fetch("https://hooks.zapier.com/hooks/catch/8026392/2b1epr8/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ line_items }),
       });
 
-      if (cardGradingItems.length > 0) {
-        // Send to Zapier for card grading purchases
-        await fetch("https://hooks.zapier.com/hooks/catch/8026392/2b1epr8/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ line_items: cardGradingItems }),
-        });
-
-        console.log("Sent card grading items to Zapier:", JSON.stringify(cardGradingItems, null, 2));
-      } else {
-        // Send other purchases to Membership webhook
-        await fetch("https://hooks.zapier.com/hooks/catch/8026392/219ausx/", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ line_items }),
-        });
-
-        console.log("Sent non-card grading items to another Zapier webhook:", JSON.stringify(line_items, null, 2));
-      }
-
+      console.log("Sent line_items to Zapier:", JSON.stringify(line_items, null, 2));
     }
 
     res.json({ received: true });
